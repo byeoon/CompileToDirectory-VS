55201c060767f3ebae4206867c8fb09459dadf10
byron <47872200+byronbytes01@users.noreply.github.com>
2023-12-20 12:23:00 -0500

    Update DirectoryChecker.php

--- CompileToDirectory.JS/DirectoryChecker.php
+++ CompileToDirectory.JS/DirectoryChecker.php
@@ -1,12 +1,24 @@
 <?php
 $directoryPath = $_POST['directoryPath'];
+    $path = realpath($directoryPath);
+    // If it exist, check if it's a directory
+    if($path !== false AND is_dir($path))
+    {
+        // Return canonicalized absolute pathname
+        return $path;
+    }
+    else
+    {
+        return false;
+    }
+
+/*
 if (is_dir($directoryPath)) {
     return 1;
 } else {
     return 0;
 }
 
-/*
 $directoryPathStable = $_POST['builds/stable'];
 $directoryPathBeta = $_POST['builds/beta'];
 
