33b9f0cfbaa55814d9f6398c5cd445a8591adf17
byron <47872200+byronbytes01@users.noreply.github.com>
2023-12-20 13:36:24 -0500

    Update Compiler.js

--- CompileToDirectory.JS/Compiler.js
+++ CompileToDirectory.JS/Compiler.js
@@ -4,22 +4,8 @@ Version: 0.0.11
 Author: byronbytes01
 */
 
+
 const directoryPath = 'builds';
-
-// Instead of the directory path being directly "/builds/YourExport", it will now split between  "/builds/beta/YourExport" and "/builds/stable/YourExport".
-const enableBetaDirectory = 'false';
-
-// Enables verbose logging to console.
-const verboseLogging = 'false';
-
-// Enables verbose logging via the 'alert' function.
-const extraVerboseLogging = 'false';
-
-
-if(extraVerboseLogging == "true")
-{
-  alert("Extra verbose logging has been enabled.");
-}
 
 
 function DoesDirectoryExist() {
