b72274dd2b6aeb38500ad4fdb3f078ffa66df2a8
byron <47872200+byronbytes01@users.noreply.github.com>
2023-12-20 12:37:50 -0500

    Create options.js

---
+++ CompileToDirectory.JS/options.js
@@ -0,0 +1,19 @@
+/* 
+Compile To Directory JS - Settings
+Author: byronbytes01
+*/
+
+/* 
+General Settings 
+*/
+
+// Instead of the directory path being directly "/builds/YourExport", it will now split between  "/builds/beta/YourExport" and "/builds/stable/YourExport".
+const enableBetaDirectory = 'false';
+
+
+/* 
+Developer Settings 
+*/
+
+// Enables verbose logging to console.
+const verboseLogging = 'false';
