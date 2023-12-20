203c782a0ba8fcdb531002f620cc2a53463d4d18
byron <47872200+byronbytes01@users.noreply.github.com>
2023-12-20 12:07:26 -0500

    Update DirectoryChecker.php

--- CompileToDirectory.JS/DirectoryChecker.php
+++ CompileToDirectory.JS/DirectoryChecker.php
@@ -2,21 +2,21 @@
 $directoryPath = $_POST['directoryPath'];
 if (is_dir($directoryPath)) {
     echo 'Default build path directory exists.';
+    return true;
 } else {
     echo 'Default build path directory does not exist.';
+    return false;
 }
 
 /*
 $directoryPathStable = $_POST['builds/stable'];
 $directoryPathBeta = $_POST['builds/beta'];
 
-
 if (is_dir($directoryPathStable)) {
     echo 'Stable build path directory exists.';
 } else {
     echo 'Stable build directory does not exist.';
 }
-
 if (is_dir($directoryPathBeta)) {
     echo 'Beta build path directory exists.';
 } else {
