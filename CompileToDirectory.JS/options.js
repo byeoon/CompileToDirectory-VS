6d3a17891e8dda6c5cb43b144566a3767ced0d5c
byron <47872200+byronbytes01@users.noreply.github.com>
2023-12-20 14:10:28 -0500

    Update options.js

--- CompileToDirectory.JS/options.js
+++ CompileToDirectory.JS/options.js
@@ -1,25 +1,12 @@
 /* 
 Compile To Directory JS - Options
-Author: byronbytes01
-*/
-
-/* 
-General Settings 
 */
 
 // Instead of the directory path being directly "/builds/YourExport", it will now split between  "/builds/beta/YourExport" and "/builds/stable/YourExport".
-const enableBetaDirectory = 'false';
+const enableBetaPath = 'false';
 
 // Instead of relying on web directories, it relies on your local computer directories / filepaths.
-const webMode = 'true';
-
-
-/* 
-Developer Settings 
-*/
+const webMode = 'true'; // For now this should be kept ON.
 
 // Enables verbose logging to console.
 const verboseLogging = 'false';
-
-// Enables verbose logging via the 'alert' function.
-const extraVerboseLogging = 'false';
